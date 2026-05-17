3d36c0274df48f947ce78f3822e666781314efe6
Raphael Tran <raphaeltrann@hotmail.com>
2026-05-17 17:08:29 +0200

    feat(src): added database + core quests commands
    Added PickleDb module + commands to add, read & remove quests
    
    BREAKING CHANGE: https://github.com/shunbolt/DT-Archie/issues/4

---
+++ src/database/database_module.py
@@ -0,0 +1,125 @@
+import os
+from pickledb import PickleDB
+
+def create_database(path_database : str, name_database : str) -> PickleDB :
+    """Function to create the database from the specified json file
+
+    Args:
+        path_database (str): Path to the database
+        name_database (str): Name of the database file
+
+    Returns:
+        PickleDB: PickleDB object representing the database
+    """
+    
+    # Build database object for the bot
+    os.makedirs(name=path_database, exist_ok=True)
+    return PickleDB(os.path.join(path_database, name_database))
+
+async def get_quests_from_user(database : PickleDB,
+                               server_id : str,
+                               user_id : str) -> list :
+    """Function to retrieve the list of all quests of a given user
+
+    Args:
+        database (PickleDB): Database to store quest info
+        server_id (str): Discord server id where the command comes from
+        user_id (str): User id where the command comes from
+
+    Returns:
+        list: List of the quests given in a list of dict format 
+    """
+    
+    await database.load()
+    
+    key = server_id + "_" + user_id
+    
+    list_to_return = await database.get(key, default=[])
+    
+    return list_to_return
+
+async def insert_quest(database : PickleDB,
+                 server_id : str,
+                 user_id : str,
+                 server_name : str,
+                 user_name : str,
+                 quest_category = "dungeon",
+                 quest_label = "",
+                 quest_comments = ""):
+    """Function to insert a quest in the given database
+
+    Args:
+        database (PickleDB): Database to store quest info
+        server_id (str): Discord server id where the command comes from
+        user_id (str): User id where the command comes from
+        server_name (str) : Discord server name where the command comes from
+        user_name (str) : User name where the command comes from
+        quest_category (str, optional): Category of quest among set values. Defaults to "dungeon".
+        quest_label (str, optional): Label of quest. Defaults to "".
+        quest_comments (str, optional): User defined comment. Defaults to "".
+    """
+    
+    await database.load()
+    
+    dict_to_insert = {
+        "server_name" : server_name,
+        "user_name" : user_name,
+        "quest_category" : quest_category,
+        "quest_label" : quest_label,
+        "quest_comments" : quest_comments
+    }
+    
+    list_quests = await get_quests_from_user(
+        database=database,
+        server_id=server_id,
+        user_id=user_id
+    )
+    
+    list_quests.append(dict_to_insert)
+    
+    # FIXME key value redundant with get_quests_from_user : use a class instead ?
+    key = server_id + "_" + user_id
+    await database.set(key, list_quests)
+    
+    await database.save()
+    
+
+async def remove_quest_from_index(database : PickleDB,
+                                  server_id : str,
+                                  user_id : str,
+                                  idx : int) -> int:
+    """Function to remove a quest from the user given it's index
+
+    Args:
+        database (PickleDB): Database to store quest info
+        server_id (str): Discord server id where the command comes from
+        user_id (str): User id where the command comes from
+        idx (int): Index to remove
+    Returns:
+        int: Status code 
+    """
+    
+    await database.load()
+    
+    list_quests = await get_quests_from_user(
+        database=database,
+        server_id=server_id,
+        user_id=user_id
+    )
+    
+    # Handle out of bound list condition
+    if idx < len(list_quests):
+        del list_quests[idx]
+    else:
+        return -1
+    
+    # FIXME key value redundant with get_quests_from_user : use a class instead ?
+    key = server_id + "_" + user_id
+    await database.set(key, list_quests)
+    
+    await database.save()
+    
+    return 0
+    
+    
+    
