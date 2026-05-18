c0bdc39e4984c4979a82b3208dc4e450ca5607d1
Raphael Tran <raphaeltrann@hotmail.com>
2026-05-19 00:22:06 +0200

    feat(src): labels + help command
    added labels for better message readability + emotes + help command from ajout_quete command
    
    https://github.com/shunbolt/DT-Archie/issues/12 \n https://github.com/shunbolt/DT-Archie/issues/15

--- src/commands/commands_module.py
+++ src/commands/commands_module.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from pickledb import PickleDB
 
 from src.database import database_module 
+from src.label import label_module
 
 HELPER_ROLE_NAME = "Helper"
 
@@ -18,15 +19,17 @@ def bot_commands(bot : commands.Bot, database : PickleDB):
     # Archie command 
     # Command to insert a quest to the database
     @bot.command()
-    async def ajout_quete(ctx : commands.Context, arg1 = "", arg2 = "", arg3 = ""):
+    async def ajout_quete(ctx : commands.Context, quest_label = None, quest_comments = ""):
         """Command to add a quest to the billboard
 
         Args:
             ctx (discord.Context): discord context of the command
-            arg1 (str): first string argument
-            arg2 (str): second string argument
-            arg3 (str): third string argument
+            quest_label (str): first argument corresponding to the label
+            If help label is passed, display the list of all possible labels and bosses
+            quest_comments (str): second argument as free comments. Defaults to ""
         """
+        # Define help labels
+        help_labels = ["help", "aide", None]
         
         # Get credentials from the discord context 
         server_id = str(ctx.guild.id)
@@ -34,29 +37,61 @@ def bot_commands(bot : commands.Bot, database : PickleDB):
         server_name = ctx.guild.name
         user_name = ctx.author.name
         
-        # HACK : Deduce category and label based on either command launched or argument passed
-        # Use typing Literal with elements from a list
-        quest_category = arg1
-        quest_label = arg2
-        quest_comments = arg3
-        
-        await database_module.insert_quest(
-            database=database,
-            server_id=server_id,
-            user_id=user_id,
-            server_name=server_name,
-            user_name=user_name,
-            quest_category=quest_category,
-            quest_label=quest_label,
-            quest_comments=quest_comments
-        )
-        
-        await ctx.reply(f"Mercenaire {ctx.author.mention} a ajouté la quête suivante sur le bulletin de la guilde : {quest_category} - {quest_label} - {quest_comments}")
+        if (quest_label in help_labels):
+            # Read list of all labels
+            dict_label = label_module.read_full_label()
+            
+            # Get list of different tag type
+            set_tags_in_dict = list(dict.fromkeys([el.get("tag") for el in dict_label.values()]))
+            
+            first_embed = True
+            for current_tag in set_tags_in_dict:
+                # Build list of elements if it belongs to the tag
+                list_label_to_display = [f"{label_module.read_tag(value.get('tag'))["discord_emoji"]} **{key}** :arrow_right: {value.get('name')}" 
+                                         for key, value in dict_label.items() if value.get('tag') == current_tag]
+                
+                # Build description and embed from it
+                description = "\n\n".join(list_label_to_display)
+                
+                # display title on the first embed sent
+                if first_embed:
+                    title = "Labels possible pour la commande `!ajout_quete <label>`"
+                    first_embed = False
+                else:
+                    title = None
+                embed = discord.Embed(title=title,
+                                    description=description)
+                
+                await ctx.send(embed=embed)
+        else:
+            # Read category based on label name
+            label_dict = label_module.read_label(quest_label)
+            
+            if label_dict is not None:
+                
+                quest_category = label_dict["category"]
+                
+                await database_module.insert_quest(
+                    database=database,
+                    server_id=server_id,
+                    user_id=user_id,
+                    server_name=server_name,
+                    user_name=user_name,
+                    quest_category=quest_category,
+                    quest_label=quest_label,
+                    quest_comments=quest_comments
+                )
+                
+                category_translated = label_module.read_category(quest_category)["name"]
+                
+                await ctx.reply(f"""Le ou la mercenaire {ctx.author.mention} a ajouté la quête suivante sur le comptoir de quêtes de la guilde :\nCatégorie : Quête {category_translated}\nNom : {label_dict["name"]}\nCommentaires : {quest_comments}""")
+            else:
+                await ctx.send(f"""Aucune quête n'a été trouvée avec le libellé : {quest_label}""")
     
     # Command to get all quests from the current user
     @bot.command()
     async def lire_quetes(ctx : commands.Context): 
-        """Function to read quests from the user on the billboard
+        """Function to read quests from the user on the quest billboard
 
         Args:
             ctx (discord.Context): discord context of the command
@@ -70,16 +105,26 @@ def bot_commands(bot : commands.Bot, database : PickleDB):
                                                    user_id=user_id)
         
         # Build embed with the quests information
-        embed = discord.Embed(title=f"Quêtes quotidiennes de {ctx.author.name} :")
+        embed = discord.Embed(title=f"Quêtes de {ctx.author.name} sur le comptoir de quêtes de la guilde :")
         embed.set_thumbnail(url=ctx.author.avatar.url)
         
         for index, quest in enumerate(list_quests):
             quest_index = index + 1
-            quest_category = quest["quest_category"]
-            quest_label = quest["quest_label"]
+            
+            quest_category = label_module.read_category(quest["quest_category"])
+            quest_category_name = quest_category["name"]
+            quest_category_emoji = quest_category["discord_emoji"]
+            quest_label = label_module.read_label(quest["quest_label"])["name"]
             quest_comments = quest["quest_comments"]
             
-            embed.add_field(name=f":crossed_swords: Quête {quest_category} #{quest_index}", value=f":label: Libellé : {quest_label}\n:pencil: Commentaires : {quest_comments}", inline=False)
+            # HACK : Function to add emoji based on difficulty tag : implement on a dict directly
+            quest_tag = label_module.read_label(quest["quest_label"])["tag"]
+            quest_tag_name = label_module.read_tag(quest_tag)["name"]
+            quest_tag_emoji = label_module.read_tag(quest_tag)["discord_emoji"]            
+            
+            embed.add_field(name=f"{quest_category_emoji} Quête {quest_category_name} {quest_tag_name} #{quest_index}",
+                            value=f""":label: Nom : {quest_label} {quest_tag_emoji}\n:pencil: Commentaires : {quest_comments}""",
+                            inline=False)
             
         await ctx.reply(embed=embed)
         
--- src/label/label_module.py
+++ src/label/label_module.py
@@ -1,7 +1,25 @@
+import os
 import json
 
-PATH_TO_LABEL_FR = "FR_label.json"
-PATH_TO_CATEGORY_FR = "FR_category.json"
+SCRIPT_PATH = os.path.dirname(__file__)
+
+PATH_TO_LABEL_FR = os.path.join(SCRIPT_PATH, "FR_label.json")
+PATH_TO_CATEGORY_FR = os.path.join(SCRIPT_PATH, "FR_category.json") 
+PATH_TO_TAG_FR = os.path.join(SCRIPT_PATH, "FR_tag.json") 
+
+def read_full_label(lang = "FR") -> list:
+    """Read the entire label file
+
+    Args:
+        lang (str, optional): Language to use. Defaults to "FR".
+
+    Returns:
+        list: List of dict contained in the full file
+    """
+    with open(PATH_TO_LABEL_FR, 'r', encoding='utf-8') as file:
+        label_data = json.load(file)
+        
+    return label_data
 
 def read_label(label : str, lang = "FR") -> dict:
     """Read corresponding label and return dict value associated with it
@@ -9,22 +27,40 @@ def read_label(label : str, lang = "FR") -> dict:
     Args:
         label (str): key label
         lang (str): Language to use. Defaults "FR"
+    Returns:
+        dict : dict of values from the label. Returns None if not found
     """
     
-    with open(PATH_TO_LABEL_FR, 'r', encoding='utf-8') as file:
-        label_data = json.load(file)
+    label_data = read_full_label(lang)
         
     return label_data.get(label)
     
-def read_category(category : str, lang = "FR") -> str:
-    """Read corresponding category and return string label value
+def read_category(category : str, lang = "FR") -> dict:
+    """Read corresponding category and return dict label value
 
     Args:
         category (str): key category
         lang (str): Language to use. Defaults "FR"
+    Returns:
+        dict : dict value of the category. Returns None if not found
     """
     
     with open(PATH_TO_CATEGORY_FR, 'r', encoding='utf-8') as file:
         category_data = json.load(file)
         
     return category_data.get(category)
+
+def read_tag(tag : str, lang = "FR") -> dict:
+    """Read corresponding tag and return dict label value
+
+    Args:
+        tag (str): key tag
+        lang (str): Language to use. Defaults "FR"
+    Returns:
+        dict : dict value of the tag. Returns None if not found
+    """
+    
+    with open(PATH_TO_TAG_FR, 'r', encoding='utf-8') as file:
+        tag_data = json.load(file)
+        
+    return tag_data.get(tag)
